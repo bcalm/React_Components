6dcf8c788bacce9e6ddfc5ae58ee9b1c8d9666c2
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-05-10 18:55:03 +0530

    Added Fragment.js
    It will not create any extra div for returning two or more than two
    child component.

---
+++ Fragment/Fragment.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Fragment extends Component {
+  render() {
+    return <React.Fragment>FragmentInput</React.Fragment>;
+  }
+}
+
+export default Fragment;
+
