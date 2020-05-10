f74dbbfd03b3a4b5159be63c02d892c3e9f1d70b
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-05-10 16:12:22 +0530

    Added FocusInput.js and Input.js file. Refs with class

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Form from './Component/Form';
 import ParentComponent from './Component/ParentComponent';
 import RefsDemo from './Component/RefsDemo';
+import FocusInput from './Component/FocusInput';
 
 class App extends Component {
   render() {
@@ -11,7 +12,8 @@ class App extends Component {
       <div>
         {/* <Form /> */}
         {/* <ParentComponent /> */}
-        <RefsDemo />
+        {/* <RefsDemo /> */}
+        <FocusInput />
       </div>
     );
   }
